6e5c1db7df054a546f1ce1d6dd51bc746e78d4d8
alpha <alphashuro@gmail.com>
2017-08-13 14:42:30 +0200

    add sections

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+import Challenge from './Challenge'
+import Editor from './Editor'
+import Tests from './Tests'
+import Output from './Output'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="App flex-grid">
+        <div className="col">
+          <Challenge />
+          <Tests />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="col">
+          <Editor />
+          <Output />
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
